d84ba116740c1533915ed1558a700bcd629ef286
José Sánchez-Gallego <gallegoj@uw.edu>
2021-04-06 22:00:02 -0700

    Update test_expose

--- tests/controller/test_expose.py
+++ tests/controller/test_expose.py
@@ -6,6 +6,7 @@
 # @Filename: test_expose.py
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 
+import asyncio
 from unittest.mock import AsyncMock
 
 import pytest
@@ -17,20 +18,42 @@ from archon.exceptions import ArchonControllerError
 pytestmark = [pytest.mark.asyncio]
 
 
+@pytest.mark.commands([["FRAME", ["<{cid}WBUF=3 BUF3COMPLETE=0"]]])
 async def test_expose(controller: ArchonController, mocker):
     set_param_mock: AsyncMock = mocker.patch.object(
         ArchonController,
         "set_param",
         wraps=controller.set_param,
     )
-    await controller.expose(2)
+    task = await controller.expose(0.01)
     set_param_mock.assert_any_call("Exposures", 1)
-    set_param_mock.assert_any_call("IntMS", 2000)
+    set_param_mock.assert_any_call("IntMS", 10)
+
     assert controller.status & ControllerStatus.EXPOSING
     assert controller.status & ControllerStatus.READOUT_PENDING
+
+    await task
+    assert controller.status == ControllerStatus.READING
 
 
 async def test_expose_not_idle(controller: ArchonController):
     controller.status = ControllerStatus.READOUT_PENDING
     with pytest.raises(ArchonControllerError):
         await controller.expose(2)
+
+
+@pytest.mark.commands([["FRAME", ["<{cid}WBUF=3 BUF3COMPLETE=1"]]])
+async def test_expose_fails_reading(controller: ArchonController, mocker):
+    mocker.patch.object(ArchonController, "set_param", wraps=controller.set_param)
+    task = await controller.expose(0.01)
+
+    with pytest.raises(ArchonControllerError):
+        await task
+
+
+async def test_expose_no_readout(controller: ArchonController, mocker):
+    mocker.patch.object(ArchonController, "set_param", wraps=controller.set_param)
+    task = await controller.expose(0.01, readout=False)
+
+    await task
+    assert controller.status & ControllerStatus.READOUT_PENDING
