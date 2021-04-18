75a50a416a766d9bdfdca59f563d04b8a1ba03e1
José Sánchez-Gallego <gallegoj@uw.edu>
2021-04-17 17:31:47 -0700

    Handle signals and gently stop the actor in __main__

--- archon/__main__.py
+++ archon/__main__.py
@@ -6,7 +6,10 @@
 # @Filename: __main__.py
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 
+import asyncio
 import os
+import signal
+from contextlib import suppress
 
 import click
 from click_default_group import DefaultGroup
@@ -16,6 +19,16 @@ from sdsstools.daemonizer import DaemonGroup
 
 import archon as archonmod
 from archon.actor.actor import ArchonActor
+
+
+async def shutdown(signal, loop, actor):
+    """Cancel tasks, including run_forever()."""
+
+    tasks = [t for t in asyncio.all_tasks() if t is not asyncio.current_task()]
+    [task.cancel() for task in tasks]
+
+    with suppress(asyncio.CancelledError):
+        await asyncio.gather(*tasks)
 
 
 @click.group(cls=DefaultGroup, default="actor", default_if_no_args=True)
@@ -52,14 +65,29 @@ async def actor(ctx):
     default_config_file = os.path.join(os.path.dirname(__file__), "etc/archon.yml")
     config_file = ctx.obj["config_file"] or default_config_file
 
-    archon_obj = ArchonActor.from_config(config_file)
+    archon_actor = ArchonActor.from_config(config_file)
 
     if ctx.obj["verbose"]:
-        archon_obj.log.fh.setLevel(0)
-        archon_obj.log.sh.setLevel(0)
+        archon_actor.log.fh.setLevel(0)
+        archon_actor.log.sh.setLevel(0)
 
-    await archon_obj.start()
-    await archon_obj.run_forever()
+    loop = asyncio.get_event_loop()
+
+    signals = (signal.SIGHUP, signal.SIGTERM, signal.SIGINT)
+    for s in signals:
+        loop.add_signal_handler(
+            s,
+            lambda s=s: asyncio.create_task(shutdown(s, loop, archon_actor)),
+        )
+
+    try:
+        await archon_actor.start()
+        await archon_actor.run_forever()
+    except asyncio.CancelledError:
+        pass
+    finally:
+        await archon_actor.stop()
+        loop.stop()
 
 
 if __name__ == "__main__":
