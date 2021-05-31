100395ded0a55a33aa2ac6cfbac99240813adcd0
José Sánchez-Gallego <gallegoj@uw.edu>
2021-05-31 14:26:38 -0700

    Return True if shutter does not need to move

--- archon/lvm/delegate.py
+++ archon/lvm/delegate.py
@@ -74,7 +74,7 @@ class LVMExposeDelegate(ExposureDelegate["LVMActor"]):
         expose_data = self.expose_data
 
         if expose_data.exposure_time == 0 or expose_data.flavour in ["bias", "dark"]:
-            return False
+            return True
 
         action = "open" if open else "close"
 
